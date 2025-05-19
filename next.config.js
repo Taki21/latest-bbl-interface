5ad7317d7ffdb4ec29ab66102098798ebf1e03c9
Taki <shakman21@gmail.com>
2025-05-19 00:32:28 -0400

    ???

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+module.exports = {
+    typescript: {
+      // ⛔️  Skip type-checking during `next build`
+      ignoreBuildErrors: true,
+    },
+  };
+  
